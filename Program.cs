7abae3b0acc55839fd72cd515678f0c41274f110
Opilane TTHK <opilane@tthk.ee>
2020-10-08 11:28:24 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace week6functions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+        }
+        public static void helloUser()
+        {
+            Console.WriteLine("enter your name:");
+            string username = Console.ReadLine();
+        Console. WriteLine($"hello ,{username}");
+        }
+
+    }
+}
